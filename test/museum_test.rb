9a0580a5fc1715ef9eaa1c06893898b67a384b69
Steven Meyers <sjjmeyers@gmail.com>
2020-02-19 09:43:40 -0700

    Add exhibits attribute to museum class

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -15,8 +15,9 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "Denver Museum of Nature and Science", @dmns.name
-    assert_equal [], @dmns.exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal [], dmns.exhibits
   end
 
 
