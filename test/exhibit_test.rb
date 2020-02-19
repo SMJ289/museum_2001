747eae2d534bcc50393b9effbfac557d322f8cbe
Steven Meyers <sjjmeyers@gmail.com>
2020-02-19 09:16:01 -0700

    Add init method and attr_reader methods

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -10,14 +10,13 @@ class ExhibitTest < Minitest::Test
   end
 
   def test_it_exists
-    exhibit = Exhibit.new
-    assert_instance_of Exhibit, exhibit
+    assert_instance_of Exhibit, @exhibit
   end
 
   def test_it_has_attributes
 
-    assert_equal "Gems and Minerals", @exhibit[:name]
-    assert_equal 0, @exhibit[:cost]
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
   end
 
 
